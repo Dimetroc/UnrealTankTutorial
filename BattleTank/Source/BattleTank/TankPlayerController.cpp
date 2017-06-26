efe89acd11c61f0c16339716c0f433d1e5a38ab9
Dmytro Aleksiuk <d.aleksiuk@ameria.de>
2017-06-26 16:16:29 +0300

    - ading AI class and Tank class

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,5 +3,26 @@
 #include "TankPlayerController.h"
 
 
+ATank* ATankPlayerController::GetControlledTank()const
+{
+	return Cast<ATank>(GetPawn());
+}
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if(ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank pawn %s found"), *(ControlledTank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank pawn not found!"));
+	}
+
+	
+}
 
